e09c57fd06f99bcc22eacca95fe20969399f0581
Jomar Yanos <jomaryanos@gmail.com>
2023-10-30 10:49:00 -0700

    merge options edit

--- backend/db/migrations/20231030033654-create-review.js
+++ backend/db/migrations/20231030033654-create-review.js
@@ -1,4 +1,12 @@
 'use strict';
+
+let options = {};
+options.tableName = "Reviews";
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -29,9 +37,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    },options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    await queryInterface.dropTable('Reviews', options);
   }
 };
