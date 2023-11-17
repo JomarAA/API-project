794de364f5e7302440e34c6865d690dcd4d4b622
Jomar Yanos <jomaryanos@gmail.com>
2023-11-17 13:53:11 -0800

    authme complete

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,48 +1,25 @@
+// frontend/src/components/Navigation/Navigation.jsx
+
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
-import SignupFormModal from '../SignupFormModal/SignupFormModal';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
+  const sessionUser = useSelector(state => state.session.user);
 
-    let sessionLinks;
-    if (sessionUser) {
-      sessionLinks = (
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && (
         <li>
           <ProfileButton user={sessionUser} />
         </li>
-      );
-    } else {
-      sessionLinks = (
-        <>
-        <li>
-          <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-            />
-        </li>
-        <li>
-          <OpenModalButton
-            buttonText="Sign Up"
-            modalComponent={<SignupFormModal />}
-            />
-        </li>
-            </>
-      );
-    }
+      )}
+    </ul>
+  );
+}
 
-    return (
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        {isLoaded && sessionLinks}
-      </ul>
-    );
-  }
-
-  export default Navigation;
+export default Navigation;
